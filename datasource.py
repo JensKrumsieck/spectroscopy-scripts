44337b2771ba6186ec06f54c08c846a5042ddc71
Jens Krumsieck <jenso666@live.de>
2022-05-04 15:26:36 +0200

    improvements

--- datasource.py
+++ datasource.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from natsort import humansorted
-
+from scipy.signal import savgol_filter
 from settings import colorMap
 
 # constants
@@ -20,7 +20,8 @@ class DataSource:
     npVTfa: float
     initialMoles: float
     moles: float
-    soret:float = 0
+    soret: float = 0
+    q: float = 0
     max: float = 0
 
     def __init__(self, folder: str, mass: float, molar: float, tfa: float):
@@ -48,49 +49,60 @@ class DataSource:
         num_spc = int(df.shape[1])
         corr = df.copy(deep=True)
         max = 0
-        idx = corr[corr.iloc[:,0].lt(400.0)].index[0]
+        idx = corr[corr.iloc[:, 0].lt(400.0)].index[0]
+        idx2 = corr[corr.iloc[:, 0].lt(800.0)].index[0]
+        self.q = corr.iloc[:idx2, 1].idxmax()
         soret = corr.iloc[:idx, 1].idxmax()
         self.soret = soret
 
         for i in range(0, num_spc, 2):
             fac = self.moles/(((i/2)+50)/1000)
-            corr.iloc[:,i+1] = corr.iloc[:,i+1]/fac
-            if max < corr.iloc[:,i+1][soret]:
-                max = corr.iloc[:,i+1][soret]
+            corr.iloc[:, i+1] = corr.iloc[:, i+1]/fac
+            if max < corr.iloc[:, i+1][soret]:
+                max = corr.iloc[:, i+1][soret]
         self.max = max
         return corr
+
     def plot(self, corr: pd.DataFrame):
         num_spc = int(corr.shape[1])
         cols = colorMap(num_spc)
-        fig,ax = plt.subplots()
+        fig, ax = plt.subplots()
         for i in range(0, num_spc, 2):
-            x = corr.iloc[:,i]
-            y = corr.iloc[:,i+1]
-            plt.plot(x,y, color=cols[int(i/2)])
-        ax.set_xlim(250,1100)
+            x = corr.iloc[:, i]
+            y = corr.iloc[:, i+1]
+            plt.plot(x, y, color=cols[int(i/2)])
+        ax.set_xlim(250, 1100)
         ax.set_ylim(0, self.max + 0.1*self.max)
         ax.set_xlabel("$\mathregular{\lambda}$ /nm")
         ax.set_ylabel("$\mathregular{\epsilon}$ /$\mathregular{L mol^{-1}cm^{-1}}$")
         plt.savefig("out/titration.png", dpi=1200)
 
-    def calculatePH(self, corr: pd.DataFrame) -> float:
-        sorets_y= corr.iloc[self.soret].iloc[1::2]
+    def calculatePH(self, corr: pd.DataFrame, idx: int) -> float:
+        sorets_y = corr.iloc[idx].iloc[1::2]
         # drop first two entries
         x_mlTFA = np.arange(0, len(sorets_y), 1)
         x_V = 50 + x_mlTFA
-        x_nTFA =  x_mlTFA * self.npVTfa
+        x_nTFA = x_mlTFA * self.npVTfa
         x_cTFA = x_nTFA/x_V
         x = -np.log10(x_cTFA)
         x = x[:-1]
         sorets_y = sorets_y[:-1]
-        x= 0.443141*np.exp((x-3.27759)/1.09351) # correction with calculated fit to pH
-        der = np.gradient(sorets_y,x) # derivate
-        min = np.nanargmin(der) # minimum
-
+        lol = len(x)
+        if(lol % 2 == 0):
+            lol = lol-1
+        sorets_y_sm = savgol_filter(sorets_y, lol, 4)  # smooth
+        x = 0.443141*np.exp((x-3.27759)/1.09351)  # correction with calculated fit to pH
         fig, ax = plt.subplots()
         ax.set_xlabel("pH")
         ax.set_ylabel("$\mathregular{\epsilon}$ /$\mathregular{L mol^{-1}cm^{-1}}$")
-        plt.plot(x, sorets_y)
-        plt.scatter(x[min], sorets_y[min])
-        plt.annotate(np.round(x[min],2),xy=(x[min], sorets_y[min]), textcoords="offset points", xytext=(5,0), fontsize=9)
-        return x[min]
+        plt.plot(x, sorets_y, label="data")
+        plt.plot(x, sorets_y_sm, label="smoothed data")
+        der = np.gradient(sorets_y_sm, x)
+        der2 = np.gradient(der, x)
+        idx = np.where(np.diff(np.sign(der2[~np.isnan(der2)])))[0][-1]
+        y = sorets_y[idx]
+        pt = x[idx]
+        plt.scatter(pt, y)
+        plt.annotate(np.round(pt, 2), xy=(pt, y), textcoords="offset points", xytext=(5, 0), fontsize=9)
+        plt.savefig("out/pH.png", dpi=1200)
+        return pt
